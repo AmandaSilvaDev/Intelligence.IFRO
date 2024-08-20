b89e586ca2cefae5d9d418bd2fecd1861b6fdb9e
AmandaSilvaDev <125752965+AmandaSilvaDev@users.noreply.github.com>
2024-08-20 19:55:12 -0400

    agora vai

---
+++ lib/src/screens/home.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+
+import 'package:chamber_deputies/src/routes/router.dart';
+
+
+class Home extends StatefulWidget {
+  const Home({super.key}); // Construtor da classe Home, que é um StatefulWidget.
+
+  @override
+  State<Home> createState() => _HomeState(); // Cria o estado para o widget Home.
+}
+
+class _HomeState extends State<Home> {
+  static const String titleAppBar = 'Início'; // Título fixo da AppBar.
+  int _selectedIndex = 0; // Índice do item selecionado na BottomNavigationBar.
+
+  // Função chamada quando um item da BottomNavigationBar é tocado.
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index; // Atualiza o índice selecionado.
+
+      // Navega para a rota correspondente ao índice selecionado.
+      switch (index) {
+        case 0:
+          Navigator.pushNamed(context, routesMap['Home']!);
+          break;
+        case 1:
+          Navigator.pushNamed(context, routesMap['deputados']!);
+          break;
+        case 2:
+          Navigator.pushNamed(context, routesMap['comissoes']!);
+          break;
+        case 3:
+          Navigator.pushNamed(context, routesMap['sobre']!);
+          break;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true, // Centraliza o título na AppBar.
+        title: const Text(
+          titleAppBar, // Exibe o título da AppBar.
+          style: TextStyle(
+            color: Colors.white, // Define a cor do texto como branco.
+            fontSize: 20, // Define o tamanho da fonte.
+            fontWeight: FontWeight.bold, // Define o peso da fonte como negrito.
+          ),
+        ),
+      ),
+      body: const HomeContent(), // Corpo da tela, exibindo o widget HomeContent.
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          // Itens da BottomNavigationBar.
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home), // Ícone do item "Início".
+            label: 'Início',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person), // Ícone do item "Deputados".
+            label: 'Deputados',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.class_), // Ícone do item "Comissões".
+            label: 'Comissões',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.info), // Ícone do item "Sobre".
+            label: 'Sobre',
+          ),
+        ],
+        currentIndex: _selectedIndex, // Índice do item atualmente selecionado.
+        selectedItemColor: Colors.green, // Cor do item selecionado.
+        unselectedItemColor: Colors.grey, // Cor dos itens não selecionados.
+        backgroundColor: Colors.white, // Cor de fundo da BottomNavigationBar.
+        onTap: _onItemTapped, // Função chamada ao tocar em um item.
+      ),
+    );
+  }
+}
+
+// Widget para exibir o conteúdo da tela inicial.
+class HomeContent extends StatelessWidget {
+  const HomeContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color.fromRGBO(254, 254, 254, 1), // Cor de fundo do Container.
+      child: ListView(
+        children: [
+          Container(
+            padding: const EdgeInsets.only(left: 8.0, right: 8.0), // Adiciona padding ao Container.
+            child: Image.asset(
+              'assets/images/logo_camara.png', // Imagem exibida na tela inicial.
+              height: 140, // Altura da imagem.
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/src/screens/sobre.dart
@@ -0,0 +1,130 @@
+import 'package:chamber_deputies/src/routes/router.dart';
+import 'package:flutter/material.dart';
+
+// Definindo uma classe chamada 'About' que herda de 'StatelessWidget'.
+// Essa classe representa uma tela estática (sem estado) do aplicativo.
+class About extends StatelessWidget {
+  // Construtor da classe 'About'.
+  const About({
+    super.key,
+  });
+
+  // Sobrescrevendo o método 'build' que constrói a interface gráfica.
+  @override
+  Widget build(BuildContext context) {
+    // Definindo uma constante para o título da AppBar.
+    const String titleAppBar = 'Sobre';
+
+    // Retornando um widget Scaffold que fornece a estrutura básica de layout.
+    return Scaffold(
+      // AppBar é a barra de título da aplicação.
+      appBar: AppBar(
+        centerTitle: true, // Centraliza o título da AppBar.
+        title: const Text(
+          titleAppBar,
+          style: TextStyle(
+            color: Colors.white, // Define a cor do texto como branco.
+            fontSize: 20, // Define o tamanho da fonte.
+            fontWeight: FontWeight.bold, // Define o peso da fonte como negrito.
+          ),
+        ),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            // Volta para a tela anterior.
+            Navigator.pushReplacementNamed(context, routesMap['Home']!);
+          },
+        ),
+      ),
+      // Corpo da tela onde o conteúdo principal é exibido.
+      body: SingleChildScrollView(
+        // Adiciona um padding (espaçamento interno) ao conteúdo.
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          // Coluna para organizar os widgets verticalmente.
+          child: Column(
+            children: [
+              // Exibe uma imagem a partir dos assets do projeto.
+              Image.asset(
+                'assets/images/logo_camara.png',
+                height: 140, // Define a altura da imagem.
+              ),
+              // Texto descritivo sobre o aplicativo.
+              const Text(
+                'É um aplicativo móvel projetado para oferecer aos usuários acesso fácil e estratégico das informações detalhadas sobre os deputados da Câmara dos Deputados do Brasil. Esperamos que ajude a fiscalizar nossos deputados e melhorar o senso crítico para o voto.',
+                textAlign: TextAlign.center, // Alinha o texto ao centro.
+                style: TextStyle(
+                  fontSize: 18, // Define o tamanho da fonte.
+                ),
+              ),
+              const SizedBox(height: 20), // Espaçamento entre widgets.
+              const Text(
+                'Desenvolvido por:',
+                style: TextStyle(
+                  fontSize: 20, // Define o tamanho da fonte.
+                  fontWeight: FontWeight.bold, // Define o peso da fonte como negrito.
+                ),
+              ),
+              const SizedBox(height: 10), // Espaçamento entre widgets.
+              
+              // Informações sobre os desenvolvedores.
+              const Column(
+                children: [
+                  CircleAvatar(
+                    radius: 50, // Define o raio do avatar circular.
+                    backgroundImage: NetworkImage(
+                      '', // URL da imagem do desenvolvedor.
+                    ),
+                  ),
+                  SizedBox(height: 10), // Espaçamento entre widgets.
+                  Text(
+                    'Amanda da Silva', // Nome do desenvolvedor.
+                    style: TextStyle(
+                      fontSize: 18, // Define o tamanho da fonte.
+                    ),
+                  ),
+                ],
+              ),
+              
+              const Column(
+                children: [
+                  CircleAvatar(
+                    radius: 50,
+                    backgroundImage: NetworkImage(
+                      '',
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'VINI',
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+              ),
+
+              const Column(
+                children: [
+                  CircleAvatar(
+                    radius: 50,
+                    backgroundImage: NetworkImage(
+                      '',
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Mestre Luan',
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
